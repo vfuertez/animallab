0488c76373d8303bb74db1d92a014c4fc9df6412
Vincent Fuertez <vfuertez@gmail.com>
2022-11-28 03:00:15 -0500

    index, show ejs created

--- controllers/animals.js
+++ controllers/animals.js
@@ -6,11 +6,41 @@ const Animal = require("../models/animals");
 
 const router = express.Router();
 
+// Error Handler
+function errorHandler(error, res) {
+  res.json(error);
+}
+
 // Routes
+
+// Index Route
+router.get("/", async (req, res) => {
+  const animals = await Animal.find({}).catch((error) =>
+    errorHandler(error, res)
+  );
+
+  res.render("animals/index.ejs", { animals });
+});
+// New Route
+
+// Destroy Route
+
+// Update Route
+
+// Create Route
+
+// Edit Route
+
+// Show route
+router.get("/:id", (req, res) => {
+  Animal.findById(req.params.id).then((animal) => {
+    res.render("animals/show.ejs", { animal });
+  });
+});
 
 // Seed Route
 router.get("/seed", async (req, res) => {
-  await Animal.remove({});
+  await Animal.remove({}).catch((error) => errorHandler(error, res));
   const animals = await Animal.create([
     {
       species: "Dolphin",
@@ -36,7 +66,7 @@ router.get("/seed", async (req, res) => {
       location: "USA",
       lifeExpectancy: 25,
     },
-  ]);
+  ]).catch((error) => errorHandler(error, res));
   res.json(animals);
 });
 
